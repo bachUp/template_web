473eeaee7c293ed25373c8d7322939b15b72a185
bachUp <anne-sophie.bach@hotmail.fr>
2015-06-07 20:30:14 +0200

    add cdn

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
 
 	<!--Bootstrap-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<!--<link rel="stylesheet" href="..\..\common\bootstrap\css\bootstrap.min.css">-->
+	<!--<link rel="stylesheet" href="..\..\common\font-awesome\css\font-awesome.min.css">-->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
 </head>
