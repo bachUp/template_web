848e1d356fed5894a774af7b03a28ee218725f88
bachUp <anne-sophie.bach@hotmail.fr>
2015-06-07 20:12:16 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8">
+
+	<title>PortFolio</title>
+
+	<!--Bootstrap-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<!--<link rel="stylesheet" href="..\..\common\bootstrap\css\bootstrap.min.css">-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<div id="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="main_menu col-md-1 animated fadeInLeft">
+					<div class="row">
+						<ul>
+							<li><a href="#" style="background:red;color:#ffffff;"><i class="glyphicon glyphicon-home"></i></li>
+							<li><a href="#" style="background:blue;color:#ffffff;"><i class="glyphicon glyphicon-list-alt"></i></li>
+							<li><a href="#" style="background:green;color:#ffffff;"><i class="glyphicon glyphicon-folder-open"></i></li>
+							<li><a href="#" style="background:yellow;color:#ffffff;"><i class="glyphicon glyphicon-envelope"></i></li>
+						</ul>
+
+					</div>
+				</div>
+				<div class="main_content col-md-10">
+					<div class="content">
+						<div class="col-md-12 pt-page-current current_page">
+							<div id ="post-1" style="border:3px solid black;">
+
+							</div>
+						</div>
+
+					</div>
+				</div>
+				<div class="network_icons col-md-1 animated fadeInRight">
+					<ul>
+						<li><a href="#" style="background:#0e5108;color:#ffffff;">lien droite</li>
+						<li><a href="#" style="background:#0e5108;color:#ffffff;">lien droite</li>
+						<li><a href="#" style="background:#0e5108;color:#ffffff;">lien droite</li>
+						<li><a href="#" style="background:#0e5108;color:#ffffff;">lien droite</li>
+						<li><a href="#" style="background:#0e5108;color:#ffffff;">lien droite</li>
+						<li><a href="#" style="background:#0e5108;color:#ffffff;">lien droite</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
